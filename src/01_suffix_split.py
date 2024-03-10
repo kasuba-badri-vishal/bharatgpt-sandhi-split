ecc62d47051619dfed9334a5aef04ad4cb9e35a5
kasuba-badri-vishal <kasubabadri@gmail.com>
2024-03-10 08:20:20 +0530

    Added Sandhi split code for hindi

---
+++ src/01_suffix_split.py
@@ -0,0 +1,91 @@
+import csv
+from aksharamukha import transliterate
+import pycdsl
+
+
+# Create an instance of CDSLCorpus321
+
+CDSL = pycdsl.CDSLCorpus()
+
+# Setup dictionaries
+dictionaries = ["MW", "AP90", "VCP", "SHS"]
+CDSL.setup(dictionaries)
+input_file = 'pratyaya.csv'#pratyaya list
+column_index_to_extract = 0  # Adjust as needed, considering 0-based indexing
+
+# Read the CSV file and extract the specified column
+with open(input_file, 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    
+    # Use list comprehension to extract the specified column
+    suffixes = [row[column_index_to_extract] for row in csv_reader]
+
+    suffixes_iso_temp = [transliterate.process('Devanagari','iso', txt) for txt in suffixes]
+
+
+
+suffixes_iso = []
+
+for suffix in suffixes_iso_temp:
+    
+    if(len(suffix) >= 3):
+   
+        for dictionary in dictionaries:
+            results = CDSL[dictionary].search(suffix)
+            if results:
+                suffixes_iso.append(suffix)
+        
+                break
+
+suffixes_iso = list(set(suffixes_iso))
+
+
+# token_file = "tokens.txt"
+data_file = "samanantar.txt"
+no_suffix_tokens=[]
+stems=[]
+suffix_freq = {key: 0 for key in suffixes_iso}
+with open(data_file,'r') as file:
+    content = file.read()
+    tokens = content.split()
+    with open("suffix_3march_final.csv", 'w', newline='') as csv_file:
+
+
+        tokens_iso = [transliterate.process('Devanagari','iso', txt) for txt in tokens]
+        for token in tokens_iso:
+            count = 0
+            for suffix in suffixes_iso:
+                if(token.endswith(suffix) and len(token[:-len(suffix)])>=3):
+
+                    # if(len(suffix) >= 3):
+                    # print(token,suffix)
+                    for dictionary in dictionaries:
+                        results = CDSL[dictionary].search(transliterate.process( 'iso','Devanagari', token[:-len(suffix)]))
+                        # vidy
+                        # print(token[4:-len(suffix)] + 'ā',results)
+                        if results:
+
+                            stems.append(transliterate.process( 'iso','Devanagari',token[:-len(suffix)] ))
+                            count = 1
+                            suffix_freq[suffix] += 1
+                            csv_writer = csv.writer(csv_file)
+                            row = [token,token[:-len(suffix)],suffix]
+                            row_hi = [transliterate.process( 'iso','Devanagari', txt) for txt in row]
+                            csv_writer.writerow(row_hi)
+                            break
+            if(count == 0):
+                no_suffix_tokens.append(transliterate.process( 'iso','Devanagari',token))
+
+
+
+# Open a new file in write mode
+with open('stems.txt', 'w') as file:
+    # Write each element to the file on a new line
+    for element in stems:
+        file.write(element + '\n')
+
+with open('no_suffix.txt', 'w') as file:
+    # Write each element to the file on a new line
+    for element in no_suffix_tokens:
+        file.write(element + '\n')
+    
---
+++ src/02_get_vibhakti.py
@@ -0,0 +1,110 @@
+import requests
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+from indic_transliteration import sanscript
+from indic_transliteration.sanscript import SchemeMap, SCHEMES, transliterate
+
+vowel_list = ["a", "aa", "i", "ii", "u", "uu", ".r", ".rr", "e", "ai", "o", "au"]
+
+def has_more_than_two_subwords(word):
+    count = 0
+    for vowel in vowel_list:
+        if vowel in word:
+            count += 1
+            if count >= 2:
+                return True
+    return False
+
+def ends_with_suffix(word, suffix_list):
+    for suffix in suffix_list:
+        if word.endswith(suffix):
+            return True
+    return False
+
+
+def get_table_result(table):
+    rows = table.find_all('tr')
+    if len(rows) > 0:
+        # Find the first row
+        first_row = rows[1]
+        # Find the first column in the first row
+        columns = first_row.find_all('th')
+        if len(columns) > 1:
+            # Get the content of the second column in the second row
+            result = columns[1].get_text(strip=True)
+        else:
+            print("No content found in the first column of the first row.")
+    else:
+        print("No rows found in the table.")
+        
+    return result
+
+#read txt and get all the words
+with open('./inputs/no_suffix.txt', 'r') as file:
+    data = file.read()
+    words = data.split("\n")
+    
+results = []
+no_table = []
+count = 0
+
+
+for word in tqdm(words):
+    
+    result = ''
+    l_velthuis=transliterate(word,sanscript.DEVANAGARI,sanscript.VELTHUIS)
+    
+    fem_words = ['aa' 'ii', 'uu', '.rr', 'e', 'ai', 'o', 'au']
+    vibakthi_type = 'Mas'
+    if(ends_with_suffix(l_velthuis, fem_words)):
+        vibakthi_type = 'Fem'
+
+
+    url = "http://10.198.63.39/cgi-bin/SKT/sktdeclin"
+    params = {
+        'lex': 'SH',
+        'q': l_velthuis,
+        't': 'VH',
+        'g': vibakthi_type,
+        'font': 'deva'
+    }
+    response = requests.get(url, params=params)
+
+    try:
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the HTML content
+            soup = BeautifulSoup(response.text, 'html.parser')
+            # Find the table
+            table = soup.find('table', class_='inflexion')
+            if table:
+                result = get_table_result(table)
+            else:
+                params = {
+                    'lex': 'SH',
+                    'q': l_velthuis,
+                    't': 'VH',
+                    'g': 'Fem',
+                    'font': 'deva'
+                }
+                response = requests.get(url, params=params)
+                soup = BeautifulSoup(response.text, 'html.parser')
+                table = soup.find('table', class_='inflexion')
+                result = get_table_result(table)
+        else:
+            print("Error:", response.status_code)
+    except:
+        result = word
+        count +=1
+                 
+    if('Fatal' in result):
+        result = word
+        count+=1
+    results.append(result)
+    
+
+print("Total Errors: ", count)
+with open('./results2/no_suffix_vibhakti.txt', 'w') as file:
+    for result in results:
+        file.write(result + "\n")
+
